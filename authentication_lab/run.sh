84dce741df9671e6e425ecb7dc1ad455d493849d
rexos <alex.pellegrini@live.com>
2013-11-15 23:40:25 +0100

    second project plus shell script code

---
+++ authentication_lab/run.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+ACN="./sample/SampleAcn.java"
+MODULE="./sample/module/SampleLoginModule.java"
+PRINCIPAL="./sample/principal/SamplePrincipal.java"
+config="./sample_jaas.config.txt"
+data="./sample/db/data.txt"
+
+if [ -f $ACN ] && 
+    [ -f $MODULE ] && 
+    [ -f $PRINCIPAL ] &&
+    [ -f $config ] &&
+    [ -f $data ]
+then
+    echo "Compiling ... "
+    javac $ACN $MODULE $PRINCIPAL
+    echo "Launching ... "
+    java -Djava.security.auth.login.config==$config sample.SampleAcn
+    echo "Cleaning ... "
+    find . -name *.class -delete
+else
+    echo "One or more required files do not exist"
+fi
