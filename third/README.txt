760d0bea851c5a2235f253bb92a26fc53a04afa7
rexos <alex.pellegrini@live.com>
2013-11-30 03:21:34 +0100

    updated statics

---
+++ third/README.txt
@@ -0,0 +1,26 @@
+Please place the two files "data.txt" and "subscriptions.txt" in
+a subdirectory with path "sample/db/"
+
+launch the script "./run.sh", it will make any necessary check in order to
+compile and launch the program, checking presence of unix utilities
+and required files. Finally it will clean the working directory 
+subtree from temporary files like *.jar and *.class, and print out
+the exit code of the program. 
+
+usernames and passwords:
+
+	  Alice - alicelive
+	  Bob - thebest
+	  Carol - google
+	  David - hackforlife
+	  Elisa - morethangod
+
+subscriptions:
+	
+	Alice - Basic
+	Bob - Basic
+	Carol - Silver
+	David - Gold
+	Elisa - Admin
+
+type "exit" to shutdown the program
--- third/run.sh
+++ third/run.sh
@@ -1,18 +1,59 @@
-#! /bin/bash
+#!/bin/bash
 
+# source codes
+action="./sample/SampleAction.java"
+azn="./sample/SampleAzn.java"
+logmod="./sample/module/SampleLoginModule.java"
+principal="./sample/principal/SamplePrincipal.java"
 
+# security data and init
+policy="./sampleazn.policy"
+config="sample_jaas.config"
 
-javac ./sample/SampleAction.java ./sample/SampleAzn.java ./sample/module/SampleLoginModule.java ./sample/principal/SamplePrincipal.java
+# static data for auth and subscriptions
+pass="./sample/db/data.txt"
+subscriptions="./sample/db/subscriptions.txt"
 
-jar -cvf SampleAzn.jar sample/SampleAzn.class sample/MyCallbackHandler.class
+# class data generted by compile
+aznclass="./sample/SampleAzn.class"
+callbackclass="./sample/MyCallbackHandler.class"
+actionclass="./sample/SampleAction.class"
+moduleclass="./sample/module/SampleLoginModule.class"
+princclass="./sample/principal/SamplePrincipal.class"
 
-jar -cvf SampleAction.jar sample/SampleAction.class
+# cleaning function
+function clean(){
+    echo "Cleaning ..."
+    find . -name "*.jar" -delete &> /dev/null
+    find . -name "*.class" -delete &> /dev/null
+    rm ./sample/db/tmp.txt &> /dev/null
+}
 
-jar -cvf SampleLM.jar sample/module/SampleLoginModule.class sample/principal/SamplePrincipal.class
+if [ ! -e $(which javac) ] ||
+    [ ! -e $(which jar) ] ||
+    [ ! -e $(which java) ]; then
+    ehco "One ore more executable ( javac, jar, java ) are missing ..."
+    exit
+else 
+    if [ ! -f $action ] ||
+	[ ! -f $azn ] ||
+	[ ! -f $logmod ] ||
+	[ ! -f $principal ] ||
+	[ \( ! -f $pass \) -o \( ! -f $subscriptions \) ]; then
+    echo "One or more file needed to compile are missing ..."
+    exit
+    else
+	echo "Compiling ... "
+	javac $action $azn $logmod $principal &> /dev/null # creates class bytecode
+	echo "Generating jars ..."
+	jar -cvf SampleAzn.jar $aznclass $callbackclass &> /dev/null # generates jar
+	jar -cvf SampleAction.jar $actionclass &> /dev/null
+	jar -cvf SampleLM.jar $moduleclass $princclass &> /dev/null
+	echo "Launching ..."
+	java -classpath SampleAzn.jar:SampleAction.jar:SampleLM.jar -Djava.security.manager -Djava.security.policy=$policy -Djava.security.auth.login.config==$config sample.SampleAzn
+    fi
+fi
 
-java -classpath SampleAzn.jar:SampleAction.jar:SampleLM.jar -Djava.security.manager -Djava.security.policy=sampleazn.policy -Djava.security.auth.login.config==sample_jaas.config sample.SampleAzn
-
-find . -name *.jar -delete &> /dev/null
-find . -name *.class -delete &> /dev/null
-rm ./sample/db/tmp.txt &> /dev/null
-ds
+status=$?
+clean
+echo "Program exited with status $status"
